7cc5ad1eae20de7a6d35a6a7d078fceaee359f62
Manshi Kumari <119393324+manshikumari12@users.noreply.github.com>
2024-02-11 16:52:48 +0530

    adding-page

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const env= require("dotenv")
 env.config()
 
-const connection = mongoose.connect("mongodb+srv://manshisbp:manshi@cluster0.vildgct.mongodb.net/taskmain?retryWrites=true&w=majority");
+const connection = mongoose.connect("mongodb+srv://manshisbp:manshi@cluster0.vildgct.mongodb.net/task?retryWrites=true&w=majority");
 
 module.exports = {
   connection,
